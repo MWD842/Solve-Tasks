07be8fdbd2837f0c54e10bac4dc54e6aa60ec342
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-01 01:55:39 -0700

    Add project files.

---
+++ SolvingTasks/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SolvingTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter your age:");
+            int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height:");
+            float userHeight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Are you a student? (Trure/False):");
+            string isStudent = Console.ReadLine();
+            Console.WriteLine($"Name: {userName}, Age: {userAge}, " +
+                $"Height: {userHeight}, Student: {isStudent}");
+        }
+    }
+}
