2c0f8cf6bce73c8f540aab5153e0ad5a67cc5c2e
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-04 22:29:00 -0700

    Movie Ticket Pricing

--- SolvingTasks/Program.cs
+++ SolvingTasks/Program.cs
@@ -86,10 +86,10 @@
             } */
 
             ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            
-            // MEDIUM (5 Tasks)
 
-            Console.WriteLine("Enter a temperature in Celsius: ");
+            // MEDIUM (5 Tasks)
+            // TASK 6: Write a C# program that takes user input for a temperature in Celsius and converts it to Fahrenheit. Display the result to the user.
+            /*Console.WriteLine("Enter a temperature in Celsius: ");
             int celsius = int.Parse(Console.ReadLine());
             int fahrenheit = (celsius * 9 / 5) + 32;
             Console.WriteLine("Temperature in Fahrenheit: " + fahrenheit);
@@ -107,12 +107,29 @@
                 default:
                     Console.WriteLine("Invalid temperature.");
                     break; 
-            } 
+            } */
 
-            
+            // TASK 7: Write a C# program that takes user input for age and determines the ticket category and price based on the following criteria.
+            Console.WriteLine("Enter the age: ");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 0 && age <= 12)
+            {
+                Console.WriteLine("Ticket Category: Children, Ticket Price: 2.000 OMR");
+            }
+            else if (age >= 13 && age <= 59)
+            {
+                Console.WriteLine("Ticket Category: Adult, Ticket Price: 5.000 OMR");
+            }
+            else if (age >= 60)
+            {
+                Console.WriteLine("Ticket Category: Senior, Ticket Price: 3.000 OMR");
+            }
+            else
+            {
+                Console.WriteLine("Invalid age.");
 
-
-        }
+            }
+}
 }
 }
 
