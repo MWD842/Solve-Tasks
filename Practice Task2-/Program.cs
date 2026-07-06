39ce4c65d468ffdc7f182f43a32cfe65f7c63ada
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-06 00:39:29 -0700

    add project files

---
+++ Practice Task2-/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice_Task2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
