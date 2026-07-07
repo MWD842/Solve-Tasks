48ef5e070bcdefb63b2782415e19ba6815c8f26d
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-07 08:10:36 -0700

    adding product files

---
+++ Practice Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice_Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
