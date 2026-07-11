526899fe77eeaee0acb8a58b3e357fe6a5fef834
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-11 13:37:46 -0700

    Fixed Menu Display Function

--- Practice Task4/Program.cs
+++ Practice Task4/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Practice_Task4
+﻿using System.Linq.Expressions;
+
+namespace Practice_Task4
 {
     internal class Program
     {
@@ -19,8 +21,40 @@
             double F = (C * 9 / 5) + 32;
             return F;
         }
+
+        public static void DisplayMenu()
+        {
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("Menu\n1) Start\n2) Help\n3) Exit");
+                Console.WriteLine("Enter your choice (1-3): ");
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                    switch(choice)
+                    {
+                        case 1:
+                            Console.WriteLine("\nStart\n");
+                            break;
+                        case 2:
+                            Console.WriteLine("\nHelp\n");
+                            break;
+                        case 3:
+                            Console.WriteLine("\nExit\n");
+                            return;
+                    }
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("\nInvlid input!!\n");
+                }
+            } while (true);
+        }
+
         static void Main(string[] args)
-        {   
+        {
             // TASK 1
             //Console.WriteLine("Enter your name: ");
             //string userName = Console.ReadLine();
@@ -31,14 +65,17 @@
             // TASK 2
             //Console.WriteLine("Enter a number to square: ");
             //int number = Convert.ToInt32(Console.ReadLine());
-            
+
             //int squaredNumber = Square(number);
             //Console.WriteLine("The square of " + number + " is: " + squaredNumber);
 
             // TASK 3
-            Console.WriteLine("Enter a temperature in Celsius: ");
-            double C = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("The temperature in Fahrenheit is: " + CelsiusToFahrenheit(C));
+            //Console.WriteLine("Enter a temperature in Celsius: ");
+            //double C = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("The temperature in Fahrenheit is: " + CelsiusToFahrenheit(C));
+
+            // TASK 4
+            DisplayMenu();
 
         }
     }
